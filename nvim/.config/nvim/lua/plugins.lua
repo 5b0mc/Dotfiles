d9a7d4960c754e9061d84fa02792c434f6c61cee
m <ms@random.cards>
2022-10-28 12:00:34 +0200

    TreeSitter cassato

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,7 +14,6 @@ use 'hrsh7th/cmp-cmdline'
 use 'hrsh7th/nvim-cmp'
 use 'hrsh7th/cmp-vsnip'
 use 'hrsh7th/vim-vsnip'
-use 'nvim-treesitter/nvim-treesitter'
 
 
 use 'gruvbox-community/gruvbox'
--- nvim/.config/nvim/lua/setups.lua
+++ nvim/.config/nvim/lua/setups.lua
@@ -97,11 +97,3 @@ cmp.setup.cmdline(':', {
   })
 })
 
-require'nvim-treesitter.configs'.setup {
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  }
-}
