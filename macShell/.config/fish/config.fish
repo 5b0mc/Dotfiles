8c136dcad3653bcfac97de95c7e41dfcc865d3c6
Mirko Sacchetti <ms@random.cards>
2023-04-15 21:49:59 +0200

    tmux is on top

--- macShell/.config/fish/config.fish
+++ macShell/.config/fish/config.fish
@@ -4,9 +4,13 @@ export TERM=xterm-256color
 
 alias vi="vim"
 alias vim="nvim"
-alias jn="jupyter notebook"
-alias wn="cd ~/Code/WiNet"
 alias newpass="openssl rand -base64 12"
+alias python="python3"
+alias smerge='/Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge .'
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl .'
+alias ms="cd ~/Writing/MirkoSacchetti"
+alias dotfiles="cd ~/Projects/dotfiles"
+alias wn="cd ~/Code/WiNet"
 
 set -g theme_nerd_fonts yes
 set fish_greeting
@@ -16,4 +20,4 @@ set -x PATH $PATH /opt/homebrew/bin
 set -x GOPATH $HOME/Code/go
 set -x PATH $PATH $GOPATH/bin
 set -x NPM_PACKAGES $HOME/.npm-packages
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $NPM_PACKAGES/bin
