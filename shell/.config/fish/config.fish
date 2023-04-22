54e897808f27e5aabb431c851d21c701eedf52a5
Mirko Sacchetti <ms@random.cards>
2023-04-22 22:53:22 +0200

    add mac stuff

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -10,12 +10,6 @@ alias ms="cd ~/Writing/MirkoSacchetti"
 alias dotfiles="nvim ~/Projects/dotfiles"
 alias wn="cd ~/Code/WiNet"
 
-if test (uname) = "Darwin"
-    set -x PATH $PATH /Users/m/Library/flutter/bin
-    set -x PATH $PATH /opt/homebrew/bin
-else if test (uname) = "Linux"
-end
-
 set -g theme_nerd_fonts yes
 set fish_greeting
 
@@ -23,8 +17,33 @@ set -x GOPATH $HOME/Code/go
 set -x NPM_PACKAGES $HOME/.npm-packages
 set -x PATH $PATH $NPM_PACKAGES/bin $GOPATH/bin
 
+if test (uname) = "Darwin"
+    set -x PATH $PATH /Users/m/Library/flutter/bin
+    set -x PATH $PATH /opt/homebrew/bin
+else if test (uname) = "Linux"
+end
+
+function pick10
+    find . -maxdepth 1  -type f -o -type d -a ! -name '.' ! -name '..' | shuf -n 10
+end
 function newpass
     for i in (seq 3)
         tr -dc 'A-Za-z0-9!@#%^&*_+-' < /dev/urandom | head -c 8; echo
     end
+end
+
+function dsstorefucker
+    sudo find ~/Code -name .DS_Store -depth -exec rm {} \;
+    sudo find ~/Projects -name .DS_Store -depth -exec rm {} \;
+    sudo find ~/Documents -name .DS_Store -depth -exec rm {} \;
+    sudo find ~/Writing -name .DS_Store -depth -exec rm {} \;
+    if test (uname) = "Darwin"
+        defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+    end 
+end  
+
+function nvmloader
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 end
