4acf91b7fe3612bc955d7f3588305144f086a983
Mirko Sacchetti <ms@random.cards>
2023-12-01 17:33:43 +0100

    test ccls

--- nvim/nvim/lua/lsp.lua
+++ nvim/nvim/lua/lsp.lua
@@ -83,23 +83,12 @@ lspconfig.tsserver.setup {
     capabilities = capabilities
 }
 
-lspconfig.pyright.setup{}
-
-
-lspconfig.clangd.setup({
+lspconfig.pyright.setup{
     capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = { "h", "c", "cpp", "cc", "objc", "objcpp"},
-    flags = lsp_flags,
-    cmd = {"clangd", "--background-index"},
-    single_file_support = true,
-    root_dir = lspconfig.util.root_pattern(
-          '.clangd',
-          '.clang-tidy',
-          '.clang-format',
-          'compile_commands.json',
-          'compile_flags.txt',
-          'configure.ac',
-          '.git'
-        )
+    on_attach = on_attach
+}
+
+lspconfig.ccls.setup({
+    capabilities = capabilities,
+    on_attach = on_attach
 })
