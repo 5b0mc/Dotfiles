f2445c199e626dbba2e14a5104a5cb5cb0d49d42
Mirko Sacchetti <ms@random.cards>
2024-01-15 00:00:14 +0100

    options cleanup

--- nvim/nvim/lua/options.lua
+++ nvim/nvim/lua/options.lua
@@ -2,39 +2,28 @@ vim.g.mapleader = ";"
 
 vim.g.netrw_banner = 0
 vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 30
 vim.g.netrw_localcopydircmd = 'cp -r'
 
 vim.opt.relativenumber = true
-vim.opt.errorbells = false
-vim.opt.updatetime = 250
 vim.opt.number = true
-vim.opt.hidden = true
-vim.opt.redrawtime = 0
-vim.opt.signcolumn="yes"
+vim.opt.errorbells = false
+vim.opt.updatetime = 300
+vim.opt.signcolumn="yes:1"
 
-vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.redrawtime = 0
-vim.opt.signcolumn="yes"
-
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.expandtab = true
 vim.opt.shortmess = vim.opt.shortmess + 'I'
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.hlsearch = false
-vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.mouse = "a"
-vim.opt.wrap = true
-vim.opt.linebreak = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt=menu,menuone,noselect
+
