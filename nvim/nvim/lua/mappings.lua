169221d50c3f11a10c1dd57e0a38999ee279aa63
Mirko Sacchetti <ms@random.cards>
2024-02-13 14:24:04 +0100

    fix vim navigation

--- nvim/nvim/lua/mappings.lua
+++ nvim/nvim/lua/mappings.lua
@@ -14,6 +14,11 @@ vim.keymap.set('n', '<Leader>w', ':w!<CR>')
 vim.keymap.set('n', '<Leader>d', ':bd<CR>')
 vim.keymap.set('x', '<Leader>p', '\"_dP')
 
+vim.keymap.set('v','j','h')
+vim.keymap.set('v','k','j')
+vim.keymap.set('v','l','k')
+vim.keymap.set('v',';','l')
+
 vim.keymap.set('n','j','h')
 vim.keymap.set('n','k','j')
 vim.keymap.set('n','l','k')
