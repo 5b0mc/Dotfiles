a1d6416b4fdde04539ccaa1e11e75d29b7df832b
Mirko Sacchetti <ms@random.cards>
2024-04-15 20:57:28 +0200

    linux update

--- nvim/nvim/lua/lsp.lua
+++ nvim/nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 local home = os.getenv( "HOME" )
 
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<space>z', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>]', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', '<space>[', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
--- nvim/nvim/lua/mappings.lua
+++ nvim/nvim/lua/mappings.lua
@@ -1,6 +1,3 @@
-vim.keymap.set('n', '<Leader>e', ':Explore<CR>')
-vim.keymap.set('i', 'kj', '<ESC>')
-
 vim.keymap.set('n', '<Leader>b', 
   "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
 vim.keymap.set('n', '<Leader>c', 
@@ -10,9 +7,13 @@ vim.keymap.set('n', '<Leader>o',
 vim.keymap.set('n', '<Leader>g', 
   "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
 
-vim.keymap.set('n', '<Leader>w', ':w!<CR>')
 vim.keymap.set('n', '<Leader>d', ':bd<CR>')
+vim.keymap.set('n', '<Leader>e', ':Explore<CR>')
+vim.keymap.set('n', '<Leader>n', ':bn<CR>')
 vim.keymap.set('x', '<Leader>p', '\"_dP')
+vim.keymap.set('n', '<Leader>w', ':w<CR>')
+
+vim.keymap.set('i', 'kj', '<ESC>')
 
 vim.keymap.set('v','j','h')
 vim.keymap.set('v','k','j')
