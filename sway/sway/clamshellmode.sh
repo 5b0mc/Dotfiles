e652a0e0a0a7d282469822446b199a3974316521
Mirko Sacchettti <ms@random.cards>
2024-11-01 10:30:18 +0100

    add mako and foot conf

--- sway/sway/clamshellmode.sh
+++ sway/sway/clamshellmode.sh
@@ -8,7 +8,12 @@ LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 read -r LS < "$LID_STATE_FILE"
 
 case "$LS" in
-*open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
-*) exit 1 ;;
+*open)
+    powerprofilesctl set balanced
+    swaymsg output "$LAPTOP_OUTPUT" enable ;;
+*closed)
+    powerprofilesctl set performance
+    swaymsg output "$LAPTOP_OUTPUT" disable ;;
+*) 
+    exit 1 ;;
 esac
