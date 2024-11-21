278e4d2de0ab51164e67ae3263e44f69054a0c3b
Mirko Sacchettti <ms@random.cards>
2024-11-21 14:32:33 +0100

    waybar

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,23 +1,27 @@
+# Fish Settings
 set fish_greeting
-export TERM=xterm-256color
-# aliases
-alias python="python3"
-alias dots="cd ~/Dotfiles && nvim ."
-alias vim="nvim"
-alias vi="vim"
-alias wn="cd ~/Code/WiNet"
-alias ms="cd ~/Writing/MirkoSacchetti"
-alias jn="jupyter notebook"
-# sunday as first day of the week is an abomination
-alias cal="cal -m"
-# envs
 set -g theme_nerd_fonts yes
 set -g TERM xterm-256color
+export TERM=xterm-256color
+# Aliases
+alias jn="jupyter notebook"
+alias python="python3"
+alias vim="nvim"
+alias vi="vim"
+# Aliases as Shortcuts for My Favorite Folders
+alias do="cd ~/Dotfiles"
+alias wn="cd ~/Code/WiNet"
+alias ms="cd ~/Writing/MirkoSacchetti"
+alias co="cd ~/Code"
+# Sunday as first day of the week is an abomination
+alias cal="cal -3m"
+# Set Environment Variables
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set MANPATH $NPM_PACKAGES/share/man $MANPATH  
 set -x GOPATH $HOME/.go
 set -x PATH $PATH $GOPATH/bin
+# Set Environment for Linux or MacOS
 set os (uname)
 switch $os
     case "Darwin"
@@ -25,17 +29,17 @@ switch $os
     case "Linux"
         set PATH $PATH /opt/nvim-linux64/bin
 end
-# stuff
+# Find a random file in the current folder
 function pickrandom
     find . -maxdepth 1 -mindepth 1 -type d | shuf -n 8
 end
-
+# Generate random srings for passwords
 function newpass
     for i in (seq 3)
         openssl rand -base64 10 | tr -d '/+=\n' | head -c 8; echo
     end
 end
-
+# Make a Quick Decision
 function coin
     if test ( math (random) % 2) -eq 0
         echo "No."
@@ -43,44 +47,3 @@ function coin
         echo "Yes."
     end
 end
-
-function dsstorefucker
-    sudo find ~/Code -name .DS_Store -depth -exec rm {} \;
-    sudo find ~/Documents -name .DS_Store -depth -exec rm {} \;
-    sudo find ~/Writing -name .DS_Store -depth -exec rm {} \;
-    if test (uname) = "Darwin"
-        defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-    end 
-end  
-
-function nvmloader
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-end
-
-function timer
-    set -l minutes 4
-    if test (count $argv) -eq 1
-        set minutes $argv[1]
-    end
-    sleep (math $minutes \* 60)
-    switch $os
-    case "macos"
-        afplay /System/Library/Sounds/Ping.aiff
-    case "linux"
-        paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-        paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-    end
-end
-
-function aj
-    mpv https://live-hls-aje-ak.getaj.net/AJE/06.m3u8 &>/dev/null &
-end
-
-
-function venv
-    python -m venv .env
-    source .env/bin/activate.fish
-end
-
