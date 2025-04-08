3d38cea26a7c8dce32798e910579a2a9e883a363
Mirko Sacchettti <ms@random.cards>
2025-04-09 00:03:31 +0200

    tmux window title

--- nvim/nvim/lua/setup.lua
+++ nvim/nvim/lua/setup.lua
@@ -28,7 +28,16 @@ require('lualine').setup {
     options = {
         theme = 'nord',
         icons_enabled = false
+    },
+    sections = {
+  lualine_c= {
+    {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
     }
+  }
+}
 }
 
 require 'fzf-lua'.setup {
